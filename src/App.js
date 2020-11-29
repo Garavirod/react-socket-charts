b85aadf0cda9dd70d22698d2078d6217c21209ad
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-28 22:13:50 -0600

    Chart.js library was added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React, { useContext } from 'react';
-import { SocketContext } from './context/SocketContext';
-import { BandList } from './components/BandList';
-import { AddBandForm } from './components/AddBand';
-
-
+import React, { useContext } from "react";
+import { SocketContext } from "./context/SocketContext";
+import { BandList } from "./components/BandList";
+import { AddBandForm } from "./components/AddBand";
+import { ChartBands } from "./components/ChartBands";
 
 function App() {
-  
   /* Extract from coentext 'online' */
-  const {online} =  useContext(SocketContext)
+  const { online } = useContext(SocketContext);
   return (
     <div className="container">
+      <h1>Graphic Bands</h1>
+      <hr />
       <div className="alert">
         <p>
           Server status:
-          {
-            (online) ? (
-              <span className="text-success ml-2">Online</span>
-            ) : 
-
-            (
-              <span className="text-danger ml-2">Offline</span>
-            )
-          }                  
+          {online ? (
+            <span className="text-success ml-2">Online</span>
+          ) : (
+            <span className="text-danger ml-2">Offline</span>
+          )}
         </p>
+        <div className="row">
+          <div className="col-md-12">
+            <ChartBands />
+          </div>
+        </div>
       </div>
-      <h1>Graphic Bands</h1>
-      <hr/>
       <div className="row">
         <div className="col-md-8">
-          <BandList/>
+          <BandList />
         </div>
         <div className="col-md-4">
-          <AddBandForm/>
+          <AddBandForm />
         </div>
       </div>
-      
     </div>
   );
 }
